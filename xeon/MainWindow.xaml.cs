999f6aa72b3cfd7ed6c50a27636b0db520efe9b0
Влад Ходаков <khodakof.v@yandex.ru>
2022-02-15 23:05:04 +0300

    123

--- xeon/MainWindow.xaml.cs
+++ xeon/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Runtime;
 
 namespace xeon
 {
@@ -24,43 +25,95 @@ namespace xeon
         {
             InitializeComponent();
         }
-        int block = 0;
-        private void bench_Click(object sender, RoutedEventArgs e)
+
+         int block = 0;
+         int TIMES = 0; //для таймера
+         double DDrun = 0;
+         double POINTS = 0;
+
+
+
+        private void bench_Click(object sender, RoutedEventArgs e) //основная грязь
         {
             String tread = Vibor.Text;
             MV.Text = tread;
             int j = Convert.ToInt32(tread);
             int i = 0;
+            TIMES = 0;
+            block = 0;
+            DDrun = 0;
+            POINTS = 0;
 
+
+            TiMers();
+            
 
             while (i != j)
             {
                 i++;
-                ras();
+                DDoub();
             }
+            
         }
 
-        async void ras()
+
+        public async void DDoub()  // double
         {
             await Task.Run(() =>
             {
                 while (true)
                 {
 
-                    double runn = 0;
-                    runn = ((runn + 1) / 2 * 3) * Math.PI;
-                    runn = ((runn + 1) / 2 * 3) * Math.PI;
-                    runn = ((runn + 1) / 2 * 3) * Math.PI;
-                    runn = ((runn + 1) / 2 * 3) * Math.PI;
-                    runn = ((runn + 1) / 2 * 3) * Math.PI;
-                    runn = ((runn + 1) / 2 * 3) * Math.PI;
+                    
+                    DDrun = (DDrun * Math.PI) / 2;
 
-                    if(block == 1)
+                    
+
+
+                    if (block == 1)
                     {
                         break;
                     }
                 }
             });
+        }
+
+        async void LLong()  // long
+        {
+            await Task.Run(() =>
+            {
+                while (true)
+                {
+
+                    long LLrun = 0;
+                    LLrun = LLrun*1*2*LLrun;
+
+
+                    if (block == 1)
+                    {
+                        break;
+                    }
+                }
+            });
+        }
+
+        async void TiMers()  // таймер(говна)
+        {
+            
+            
+                while (true)
+                {
+                    TIMES++;
+                    TimeOC.Text = TIMES.ToString();
+
+                    await Task.Delay(1000);
+
+                    if (block == 1)
+                    {
+                        break;
+                    }
+                }
+            
         }
 
         private void Sbross_Click(object sender, RoutedEventArgs e)
